495a56e8f1dd91f262bacecb6ee58f057707bf98
yangzikang <986762402@qq.com>
2017-06-05 20:15:39 +0800

    kotlin test

---
+++ app/src/main/java/edu/yangzikang/kotlintest/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.yangzikang.kotlintest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var tv= findViewById(R.id.text) as TextView
+        tv.setText("helloworld")
+        
+
+    }
+}
